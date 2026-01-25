4d6a93c83dae0b32f70197b09c771b4510c9fa66
Boitumelo <167770627+BoituXZ@users.noreply.github.com>
2026-01-25 21:00:01 +0200

    Create if_else_problem.py

---
+++ hackerRank/if_else_problem.py
@@ -0,0 +1,22 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+def number_checker(n):
+    if n % 2 != 0:
+        print("Weird")
+    else:
+        if (n>=2 and n<=5):
+            print("Not Weird")
+        elif (n>=6 and n<=5):
+            print("Weird")
+        else:
+            print("Not Weird")
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    number_checker(n)
+
